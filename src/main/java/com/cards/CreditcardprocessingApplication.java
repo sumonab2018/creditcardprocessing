0f3260165f95b57d11c37ac729882845b425106a
Sumonab2016 <sumonab2020@gmail.com>
2021-03-09 22:20:24 +0000

    Modified to fix the CreditCardService initialization error

--- src/main/java/com/cards/CreditcardprocessingApplication.java
+++ src/main/java/com/cards/CreditcardprocessingApplication.java
@@ -2,10 +2,24 @@ package com.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
+import com.cards.service.CreditCardService;
+import com.cards.service.CreditCardServiceImpl;
+
+@PropertySource("classpath:application.properties")
+@ConfigurationProperties(prefix = "app")
 @SpringBootApplication
 public class CreditcardprocessingApplication {
 
+	@Bean
+	CreditCardService getCreditCardService() {
+		return new CreditCardServiceImpl();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CreditcardprocessingApplication.class, args);
 	}
